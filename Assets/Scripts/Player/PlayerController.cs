b45fe569c5cea51bae453d49caad1404d36bd69a
Nq26rp <2389886419@qq.com>
2026-06-19 16:41:18 +0800

    完成角色移动和翻转

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public @_2Dgame inputController;
+    public Vector2 inputDirection;
+    public float Speed;
+    public Rigidbody2D rb;
+    public SpriteRenderer sr;
+
+    private void Awake()
     {
-        
+        inputController = new @_2Dgame();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    private void OnEnable()
+    {
+        inputController.Enable();
+    }
+    private void OnDisable()
+    {
+        inputController.Disable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        inputDirection = inputController.Player.Move.ReadValue<Vector2>();
+        Filp();
     }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(inputDirection.x * Speed, rb.velocity.y);
+    }
+    private void Filp()
+    {
+        if (inputDirection.x > 0)
+            sr.flipX = false;
+        else if (inputDirection.x < 0)
+            sr.flipX = true;
+    }
+
 }
